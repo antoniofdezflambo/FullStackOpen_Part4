5ee523d152d9038d76a853ef6cd768aaca6b3009
antoniofdezflambo <antoniofdezflambo@gmail.com>
2024-05-07 00:30:09 +0200

    Exercise 4.8 completed

---
+++ backend_blog/tests/blog_api.test.js
@@ -0,0 +1,30 @@
+const { test, after, describe, beforeEach } = require('node:test')
+const assert = require('assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('blog api tests', () => {
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
