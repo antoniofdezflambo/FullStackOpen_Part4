620a2d162346aaa6572feb9b2a96aa46c7f996de
antoniofdezflambo <antoniofdezflambo@gmail.com>
2024-05-07 02:09:28 +0200

    Fixed merge errors

--- backend_blog/controllers/blogs.js
+++ backend_blog/controllers/blogs.js
@@ -10,6 +10,8 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
+    if(!body.title || !body.url) response.status(400).end()
+
     const blog = new Blog({
         title: body.title,
         author: body.author,
