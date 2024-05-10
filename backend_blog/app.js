17ed5eb3d987ff97b0266aa020013f2a13b5796b
antoniofdezflambo <antoniofdezflambo@gmail.com>
2024-05-10 17:47:33 +0200

    Exercise 4.18 completed

--- backend_blog/app.js
+++ backend_blog/app.js
@@ -1,12 +1,15 @@
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
-const config = require('./utils/config')
+const loginRouter = require('./controllers/login')
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
@@ -27,6 +30,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
